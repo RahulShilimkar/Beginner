a1a2962a1f85903be20f705fa60c3536bad67ce1
RahulShilimkar <silimkarr@gmail.com>
2022-12-25 10:32:53 +0530

    First Commit

---
+++ IneuronAI/src/basicPack/Identifier.java
@@ -0,0 +1,15 @@
+package basicPack;
+
+public class Identifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Sachin";
+		String result = "Noob";
+		System.out.println(name);
+		System.out.println(result);  	//14 identifier's --> ClassName,MethodName and VariableName.
+
+	}
+
+}
